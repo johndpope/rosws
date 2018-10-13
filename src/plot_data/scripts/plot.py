39a8165b633b8312bba62ccb61d935c5c4dabbb6
Unknown <1058088476@qq.com>
2018-10-13 22:13:47 +0800

    完善plot_data
    完善刻度显示

--- src/plot_data/scripts/plot.py
+++ src/plot_data/scripts/plot.py
@@ -7,7 +7,7 @@ import matplotlib.animation as animation
 import rospy
 from std_msgs.msg import String
 
-newdata = 5.55
+newdata = 0.00
 flag = True
 
 def callback(imgmsg):
@@ -15,15 +15,21 @@ def callback(imgmsg):
     global newdata
     flag = True
     newdata = float(imgmsg.data.split(",")[0])
+    if(newdata > 180):
+        newdata = newdata - 360
     rospy.loginfo("newdata is %s", newdata)
     flag = False
 
-num =[0.0 for _ in range(60)]
-xaxes = [x for x in range(0, 60)]
+num =[0.0 for _ in range(600)]
+xaxes = [x for x in range(0, 600)]
 fig = plt.figure() 
 axes1 = fig.add_subplot(111) 
 line, = axes1.plot(xaxes, num) 
-axes1.set_ylim(-180, 180) #设置y轴范围
+axes1.set_ylim(-181, 181) #设置y轴范围
+axes1.set_yticks([-180, -170, -160, -150, -140, -130, -120, -110, -100,
+                  -90, -80, -70, -60, -50, -40, -30, -20, -10, 0,
+                  10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+                  110, 120, 130, 140, 150, 160, 170, 180])
 plt.xticks([]) #关闭x轴刻度
 
 def update(data): 
